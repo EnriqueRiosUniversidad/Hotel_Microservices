57900a515748c099dd29fe0ec40444fa546bb43f
Enrique Emmanuel Rios Chyrnia <enrique.rios2019@fiuni.edu.py>
2024-09-10 16:54:33 -0400

    prueba

---
+++ Gestion_Usuarios/src/main/java/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package controller;
+
+
+import distri.beans.dto.UsuarioDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.UsuarioService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> crearUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        UsuarioDTO nuevoUsuario = usuarioService.crearUsuario(usuarioDTO);
+        return ResponseEntity.ok(nuevoUsuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> obtenerUsuarios() {
+        return ResponseEntity.ok(usuarioService.obtenerUsuarios());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> obtenerUsuarioPorId(@PathVariable Long id) {
+        UsuarioDTO usuario = usuarioService.obtenerUsuarioPorId(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
+        usuarioService.eliminarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ Gestion_Usuarios/src/main/java/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package service;
+
+
+import distri.beans.domain.Usuario;
+import distri.beans.dto.UsuarioDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UsuarioDTO crearUsuario(UsuarioDTO usuarioDTO) {
+        Usuario usuario = modelMapper.map(usuarioDTO, Usuario.class);
+        Usuario usuarioGuardado = usuarioRepository.save(usuario);
+        return modelMapper.map(usuarioGuardado, UsuarioDTO.class);
+    }
+
+    public List<UsuarioDTO> obtenerUsuarios() {
+        return usuarioRepository.findAll()
+                .stream()
+                .map(usuario -> modelMapper.map(usuario, UsuarioDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public UsuarioDTO obtenerUsuarioPorId(Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario.map(value -> modelMapper.map(value, UsuarioDTO.class)).orElse(null);
+    }
+
+    public void eliminarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}

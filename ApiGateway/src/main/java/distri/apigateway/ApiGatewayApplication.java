d2861b3ca830c341ccb2824bf9f52b6b78039cf8
Enrique Rios Chyrnia <enrique.rios2019@fiuni.edu.py>
2024-11-20 10:51:15 -0300

    modifique el apigateway para poder crear su war apigateway

--- ApiGateway/src/main/java/distri/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/distri/apigateway/ApiGatewayApplication.java
@@ -2,18 +2,25 @@ package distri.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
-@ComponentScan(basePackages =
-        {"distri.apigateway.config","distri.apigateway.filter"})
-public class ApiGatewayApplication {
+@ComponentScan(basePackages = {
+        "distri.apigateway.config",
+        "distri.apigateway.filter"
+})
+public class ApiGatewayApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApiGatewayApplication.class);
+    }
 }
